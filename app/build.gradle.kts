bf21eb5d3d7a05d7d388a6c7779ffa1a828825cd
aichagh <aichagh2002@gmail.com>
2024-02-19 16:24:25 -0500

    Firebase support added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // for firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -66,4 +69,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-analytics")
 }
