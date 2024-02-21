c96617255644f5294bf751e1d56ddd8ae40282a0
sarazufishan <szufishan@uwaterloo.ca>
2024-02-20 20:30:06 -0500

    splash screen

---
+++ app/src/main/java/com/criticove/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.criticove
+
+import android.app.Activity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : Activity() {
+    private val SPLASH_DELAY: Long = 2000 // 2 seconds delay
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
+    }
+
+
+
+}
