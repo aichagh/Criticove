36ee2804aa1156dfb2f96e9406f7f94732438003
reem <r3fakhou@uwaterloo.ca>
2024-02-23 19:48:15 -0500

    submit review

--- app/src/main/java/com/criticove/ReviewForm.kt
+++ app/src/main/java/com/criticove/ReviewForm.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,6 +30,10 @@ val filled = mutableMapOf(
     "Book" to mutableMapOf("Book Title" to "", "Author" to "", "Date Published" to "", "Genre" to "", "Book Type" to ""),
     "TV Show" to mutableMapOf("TV Show Title" to "", "Director" to "", "Date Released" to "", "Genre" to "", "Streaming Service" to ""),
     "Movie" to mutableMapOf("Movie Title" to "", "Director" to "" , "Date Released" to "", "Genre" to "", "Publication Company" to ""))
+
+var submittedReview: MutableMap<String, String>? = null
+
+
 class ReviewForm : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,25 +74,25 @@ fun ReviewHeader() {
 @Composable
 fun Selection() {
     var selectedType by remember { mutableStateOf("Book") }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(30.dp)
-                .padding(horizontal = 10.dp)
-                .background(colorResource(id = R.color.off_white)),
-        ) {
-            val mediaType = listOf("Book", "TV Show", "Movie")
-            mediaType.forEach{ el ->
-                RadioButton(
-                    selected = selectedType == el,
-                    onClick = { selectedType = el }
-                )
-                Text(
-                    text = el,
-                    modifier = Modifier.padding(start = 4.dp)
-                )
-            }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(30.dp)
+            .padding(horizontal = 10.dp)
+            .background(colorResource(id = R.color.off_white)),
+    ) {
+        val mediaType = listOf("Book", "TV Show", "Movie")
+        mediaType.forEach{ el ->
+            RadioButton(
+                selected = selectedType == el,
+                onClick = { selectedType = el }
+            )
+            Text(
+                text = el,
+                modifier = Modifier.padding(start = 4.dp)
+            )
         }
+    }
     CreateForm(selectedType)
 }
 @Composable
@@ -106,55 +111,64 @@ fun CreateForm(type:String) {
     ) {
         Column() {
             elements.forEach { label ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Column() {
-                            var bookData by remember {mutableStateOf("")}
-                            var movieData by remember {mutableStateOf("")}
-                            var tvData by remember {mutableStateOf("")}
-                            //Text(
-                                //text = label
-                           // )
-                           // println("this is the value: $filled[type]?.get(label)")
-                            when(type) {
-                                "Book" -> {OutlinedTextField(value=bookData,onValueChange={bookData=it},
-                                    label = {Text( text = label, color = colorResource(id = R.color.blue)) },
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                )
-                                    filled["Book"]?.set(label, bookData).toString()}
-                                "TV Show" -> {OutlinedTextField(value=tvData,onValueChange={tvData=it},
-                                    label = {Text( text = label, color = colorResource(id = R.color.blue)) },
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                )
-                                    filled["TV Show"]?.set(label, tvData).toString()}
-                                "Movie" -> {OutlinedTextField(value=movieData,onValueChange={movieData=it},
-                                    label = {Text( text = label, color = colorResource(id = R.color.blue)) },
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                )
-                                    filled["Movie"]?.set(label, movieData).toString()}
-                            }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column() {
+                        var bookData by remember {mutableStateOf("")}
+                        var movieData by remember {mutableStateOf("")}
+                        var tvData by remember {mutableStateOf("")}
+                        //Text(
+                        //text = label
+                        // )
+                        // println("this is the value: $filled[type]?.get(label)")
+                        when(type) {
+                            "Book" -> {OutlinedTextField(value=bookData,onValueChange={bookData=it},
+                                label = {Text( text = label, color = colorResource(id = R.color.blue)) },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            )
+                                filled["Book"]?.set(label, bookData).toString()}
+                            "TV Show" -> {OutlinedTextField(value=tvData,onValueChange={tvData=it},
+                                label = {Text( text = label, color = colorResource(id = R.color.blue)) },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            )
+                                filled["TV Show"]?.set(label, tvData).toString()}
+                            "Movie" -> {OutlinedTextField(value=movieData,onValueChange={movieData=it},
+                                label = {Text( text = label, color = colorResource(id = R.color.blue)) },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            )
+                                filled["Movie"]?.set(label, movieData).toString()}
                         }
                     }
+                }
             }
         }
     }
     println("this is filled $filled")
+    Submission(type)
 }
 
 @Composable
-fun Submission() {
+fun Submission(type: String) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .background(colorResource(id = R.color.off_white))
     ) {
-        Row() {
+        Column() {
+            Button(onClick = {
+                when (type) {
+                    "Book" -> submittedReview = filled["Book"]
+                    "TV Show" -> submittedReview = filled["TV Show"]
+                    "Movie" -> submittedReview = filled["Movie"]
+                }
+            }) {
 
+            }
         }
     }
 }
