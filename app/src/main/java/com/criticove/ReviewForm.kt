12e4a8abfcc80f72d0421694f6d20b7d21440bbb
aichagh <aichagh2002@gmail.com>
2024-03-01 22:16:03 -0500

    fixed text box outline + colors

--- app/src/main/java/com/criticove/ReviewForm.kt
+++ app/src/main/java/com/criticove/ReviewForm.kt
@@ -4,7 +4,9 @@ package com.criticove
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,8 +18,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
@@ -26,6 +30,7 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonColors
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.colorResource
@@ -126,6 +131,7 @@ fun Selection() {
     }
     CreateForm(selectedType)
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateForm(type:String) {
     var elements =  mutableListOf<String>()
@@ -137,7 +143,8 @@ fun CreateForm(type:String) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(colorResource(id = R.color.off_white))
+                .background(colorResource(id = R.color.off_white)),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         )
         {
             elements.forEach { label ->
@@ -146,22 +153,45 @@ fun CreateForm(type:String) {
                 var tvData by remember {mutableStateOf("")}
 
                 when(type) {
-                    "Book" -> {OutlinedTextField(value=bookData, onValueChange={bookData=it},
-                        label = {Text( text = label, color = colorResource(id = R.color.blue),) },
+                    "Book" -> { OutlinedTextField(
+                        value = bookData,
+                        onValueChange = { bookData = it },
+                        label = { Text( text = label, color = colorResource(id = R.color.coolGrey),) },
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxWidth(),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = colorResource(id = R.color.blue),
+                            unfocusedBorderColor = colorResource(id = R.color.teal)
+                        ),
+                        shape = RoundedCornerShape(10.dp)
                     )
                         filled["Book"]?.set(label, bookData).toString()}
-                    "TV Show" -> {OutlinedTextField(value=tvData,onValueChange={tvData=it},
-                        label = {Text( text = label, color = colorResource(id = R.color.blue)) },
+
+                    "TV Show" -> { OutlinedTextField(
+                        value = tvData,
+                        onValueChange = { tvData = it },
+                        label = { Text( text = label, color = colorResource(id = R.color.coolGrey),) },
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxWidth(),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = colorResource(id = R.color.blue),
+                            unfocusedBorderColor = colorResource(id = R.color.teal)
+                        ),
+                        shape = RoundedCornerShape(10.dp)
                     )
                         filled["TV Show"]?.set(label, tvData).toString()}
-                    "Movie" -> {OutlinedTextField(value=movieData,onValueChange={movieData=it},
-                        label = {Text( text = label, color = colorResource(id = R.color.blue)) },
+
+                    "Movie" -> {OutlinedTextField(
+                        value = movieData,
+                        onValueChange = { movieData = it },
+                        label = { Text( text = label, color = colorResource(id = R.color.coolGrey),) },
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxWidth(),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = colorResource(id = R.color.blue),
+                            unfocusedBorderColor = colorResource(id = R.color.teal)
+                        ),
+                        shape = RoundedCornerShape(10.dp)
                     )
                         filled["Movie"]?.set(label, movieData).toString()}
                 }
